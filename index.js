f07cb6f11f3de714914459b833d9e053c236af98
Your Name <you@example.com>
2021-06-05 21:49:06 +0200

    add scraping  creche drescription pages

--- index.js
+++ index.js
@@ -2,44 +2,45 @@ const puppeteer = require("puppeteer")
 const cheerio = require("cheerio")
 
 
-const scrapingResults = [{
-    title: "Entry level software engineer",
-    datePosted: new Date("2019-07-26 12:00:00"),
-    neighborhood: "(palo alto)",
-    url: "",
-    jobDescription: "",
-    compensation: ""
+async function scrapeListings(page) {
+  
+    await page.goto("https://ssl.education.lu/sea?UniteTypeIds=9022%2C7021%2C4021%2C4022&Nom=&Localite=Esch-sur-Alzette")
+    const html = await page.content();
+    const $ = cheerio.load(html);
 
-}];
+    //
+    // ─── SEARCH CSS CLASS IN SELECTED PAGE ──────────────────────────────────────────
+    //
+    const resultat = $(".table-striped tbody tr td a").map((index,element) => {
+        const crecheName = $(element).text();
+        const url = 'https://ssl.education.lu' + $(element).attr('href');
+        return { crecheName,url}
+    })
+    .get();
+    return resultat
+   
+}
+
+async function scrapeCrecheDescriptions(listings, page){
+    for (let i = 0; i < listings.length; i++) {
+        await page.goto(listings[i].url);
+        const html = await page.content();
+        
+    }
+}
 
 
-async function main() {
-    let hrefList = [];
+async function main(){
     const browser = await puppeteer.launch({
         headless: false
     });
     const page = await browser.newPage();
-    await page.goto("https://ssl.education.lu/sea?UniteTypeIds=9022%2C7021%2C4021%2C4022&Nom=&Localite=Esch-sur-Alzette")
-    const html = await page.content();
-    const $ = cheerio.load(html);
-    const resultat = $('.table-striped tbody tr td a')
+    const listings = await scrapeListings(page)
+    const listingsWithDescriptions = await scrapeCrecheDescriptions(listings, page)
+    console.log(listings);
 
-     for (let i = 0; i < resultat.length; i++) {
-        debugger
-        const element =  resultat[i].href;
-        const list = hrefList.push(element)
-        //console.log(list)
-    }
 
-    $(".table-striped tbody tr td a").each(function(i, item){
-        console.log($(item).attr('href'))
-    });
-    
-    //console.log(resultat[2])
 }
-
-
-
 
 
 main();
