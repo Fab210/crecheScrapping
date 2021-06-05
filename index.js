57d52488186bd728416dca1b50b5bc69a59f632d
Your Name <you@example.com>
2021-06-06 00:00:15 +0200

    create objects with all necessary infos

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cheerio = require("cheerio")
 
 
 async function scrapeListings(page) {
-  
+
     await page.goto("https://ssl.education.lu/sea?UniteTypeIds=9022%2C7021%2C4021%2C4022&Nom=&Localite=Esch-sur-Alzette")
     const html = await page.content();
     const $ = cheerio.load(html);
@@ -11,26 +11,61 @@ async function scrapeListings(page) {
     //
     // ─── SEARCH CSS CLASS IN SELECTED PAGE ──────────────────────────────────────────
     //
-    const resultat = $(".table-striped tbody tr td a").map((index,element) => {
+    const resultat = $(".table-striped tbody tr td a").map((index, element) => {
         const crecheName = $(element).text();
         const url = 'https://ssl.education.lu' + $(element).attr('href');
-        return { crecheName,url}
+        const email = ''
+        return { crecheName, url ,email}
     })
-    .get();
+        .get();
+    console.log(resultat)
     return resultat
-   
+
 }
 
-async function scrapeCrecheDescriptions(listings, page){
+async function scrapeCrecheDescriptions(listings, page) {
     for (let i = 0; i < listings.length; i++) {
         await page.goto(listings[i].url);
         const html = await page.content();
+        const $ = cheerio.load(html);
+
+
+        const resultat = $(".signaletique").map((index, element) => {
+            const adressElement = $(element).find('.col-4:first p:nth-child(2)');
+            const emailElement = $(element).find('p a:first');
+            const phoneElement = $(element).find('.col-4:nth-child(2) p:nth-child(2)');
+            const adress = $(adressElement).text().trim();
+            const email = $(emailElement).attr('href');
+            const phone = $(phoneElement).text().trim();
+            let cleanEmail = '';
+            let cleanPhone = '';
+            if (email != undefined) {
+                cleanEmail = email.replace("mailto:", "");
+            }
+            if (phone != undefined) {
+                cleanPhone = phone.replace("\n\t\t\t", " ");
+            }
+
+            const cleanAdress = adress.replace("\n\t\t\t", " ")
+listings[i].email = cleanEmail
+           // return { cleanAdress, cleanPhone, cleanEmail }
+        })
+            .get();
+
         
+        console.log(listings[i])
+        //return resultat
+        await sleep(3000)
+
     }
 }
 
+async function sleep(miliseconds) {
+    return new Promise(resolve => setTimeout(resolve, miliseconds));
+}
 
-async function main(){
+
+async function main() {
     const browser = await puppeteer.launch({
         headless: false
     });
